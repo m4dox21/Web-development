d160deaf308587804591699f9a397813952ae146
m4dox21 <m4dox21@gmail.com>
2024-06-05 17:15:34 +0200

    Add show, update and delete buttons

--- gallery/routes/images.js
+++ gallery/routes/images.js
@@ -10,7 +10,20 @@ router.get("/", image_controller.image_list);
 
 //IMAGE ADD GET
 router.get("/image_add", authenticate, image_controller.image_add_get);
+
 //IMAGE ADD POST
 router.post("/image_add", authenticate, image_controller.image_add_post);
+
+// IMAGE SHOW GET
+router.get("/:id", authenticate, image_controller.image_show_get);
+
+// IMAGE UPDATE GET
+router.get("/:id/update", authenticate, image_controller.image_update_get);
+
+// IMAGE UPDATE POST
+router.post("/:id/update", authenticate, image_controller.image_update_post);
+
+// IMAGE DELETE POST
+router.post("/:id/delete", authenticate, image_controller.image_delete_post);
 
 module.exports = router;
