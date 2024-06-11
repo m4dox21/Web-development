52bd2622b2d7f4d0d167c542b3be69dd466c2064
m4dox21 <m4dox21@gmail.com>
2024-06-11 18:40:20 +0200

    Upload test project

--- gallery/routes/stats.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const stats_controller = require("../controllers/statsController");
-
-// STATS GET (/stats)
-router.get("/", stats_controller.stats_list);
-
-module.exports = router;
